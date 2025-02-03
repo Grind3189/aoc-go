76cb7fc4532019f55ea8bdbe425534ac98a2b34c
Grind3189 <bellenlowell@gmail.com>
2025-02-04 03:42:33 +0800

    2024 d2 p2

--- 2024/02/solution.go
+++ 2024/02/solution.go
@@ -12,48 +12,18 @@ import (
 func part1(file *os.File) {
 	scanner := bufio.NewScanner(file)
 	var safe int
-outer:
+
 	for scanner.Scan() {
 		const MIN, MAX = 1, 3
 
 		text := scanner.Text()
 		input := strings.Split(text, " ")
 
-		asc, desc, prev := false, false, 0
-	check:
-		for i, txt := range input {
-			diff := 0
-			num, err := strconv.Atoi(txt)
-			if err != nil {
-				panic(err)
-			}
-
-			if i == 0 {
-				prev = num
-				continue check
-			}
-
-			// each outer loop should only be asc or desc
-			// to be considered valid
-			if num > prev && desc == false {
-				asc = true
-				diff = num - prev
-			} else if num < prev && asc == false {
-				desc = true
-				diff = prev - num
-			} else {
-				continue outer
-			}
-
-			prev = num
-
-			// the difference between each level should be between
-			// the minimum and maximum value to be considered valid
-			if diff < MIN || diff > MAX {
-				continue outer
-			}
-
+		result := checkArr(input)
+		if result == false {
+			continue
 		}
+
 		safe++
 	}
 
