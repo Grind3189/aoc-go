7a7696a542cf84bb5deebcd1bb8cb8bf2d1872f2
Grind3189 <bellenlowell@gmail.com>
2025-02-04 21:42:16 +0800

    2024 d3 p2

--- 2024/03/solution.go
+++ 2024/03/solution.go
@@ -38,12 +38,65 @@ func part1(file *os.File) {
 	fmt.Println("Part 1:", total)
 }
 
+func part2(file *os.File) {
+	scanner := bufio.NewScanner(file)
+
+	var total int
+	enable := true
+	for scanner.Scan() {
+		text := scanner.Text()
+		// find mul(nums,nums) or do() or don't()
+		re := regexp.MustCompile(`mul[(]\d+,\d+[)]|don't[(][)]|do[(][)]`)
+
+		for _, match := range re.FindAllString(text, -1) {
+			// control enable var base on the text
+			if match == "don't()" {
+				enable = false
+				continue
+			} else if match == "do()" {
+				enable = true
+				continue
+			}
+
+			// skip if operation is disabled
+			if enable == false {
+				continue
+			}
+
+			// at this point we have mul(nums,nums)
+			// get the value, convert and perform operation
+			reg := regexp.MustCompile(`\d+`)
+			strs := reg.FindAllString(match, -1)
+
+			num1, err := strconv.Atoi(strs[0])
+			if err != nil {
+				panic(err)
+			}
+			num2, err := strconv.Atoi(strs[1])
+			if err != nil {
+				panic(err)
+			}
+
+			total += num1 * num2
+
+		}
+
+	}
+
+	fmt.Println("Part 2:", total)
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-
 	part1(file)
+
+	file, err = os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	part2(file)
 }
