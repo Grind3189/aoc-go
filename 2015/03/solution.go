153f2980efbb96d37fc2884e4bd78a39a1ac0e7b
Grind3189 <bellenlowell@gmail.com>
2025-03-07 10:18:26 +0800

    2015 d3 p2

--- 2015/03/solution.go
+++ 2015/03/solution.go
@@ -22,6 +22,7 @@ func part1(file *os.File) {
 		for _, runes := range text {
 			char := string(runes)
 
+			// update x,y coordinates of santa
 			switch char {
 			case "^":
 				x++
@@ -31,10 +32,10 @@ func part1(file *os.File) {
 				y++
 			case "<":
 				y--
-			default:
-				fmt.Println("hello")
 			}
 
+			// check if the current loc from coordinates
+			// has not been visited yet, update if not
 			newLoc := strconv.Itoa(x) + "-" + strconv.Itoa(y)
 			locVal := locMap[newLoc]
 			if locVal == 0 {
@@ -44,10 +45,61 @@ func part1(file *os.File) {
 			loc = newLoc
 		}
 
-		fmt.Println("---")
+	}
+	fmt.Println("Part 1:", visited)
+}
+
+func part2(file *os.File) {
+	scanner := bufio.NewScanner(file)
+	// robo and santa x and y coordinates
+	rX, rY, sX, sY := 0, 0, 0, 0
+	visited := 1
+	roboLoc, santaLoc := "0-0", "0-0"
+	locMap := map[string]int{"0-0": 1}
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		for i, runes := range text {
+			char := string(runes)
+			var x, y *int
+			var loc *string
+
+			// determine who's moving
+			// santa if even else its robo
+			if i%2 == 0 {
+				x, y = &sX, &sY
+				loc = &santaLoc
+			} else {
+				x, y = &rX, &rY
+				loc = &roboLoc
+			}
+
+			// update x,y coordinates
+			switch char {
+			case "^":
+				*x++
+			case "v":
+				*x--
+			case ">":
+				*y++
+			case "<":
+				*y--
+			}
+
+			// check if the current loc from coordinates
+			// has not been visited yet, update if not
+			newLoc := strconv.Itoa(*x) + "-" + strconv.Itoa(*y)
+			locVal := locMap[newLoc]
+			if locVal == 0 {
+				visited++
+			}
+			locMap[newLoc] = locVal + 1
+			*loc = newLoc
+		}
 	}
 
-	fmt.Println("Part 1:", visited)
+	fmt.Println("Part 2:", visited)
 }
 
 func main() {
@@ -55,6 +107,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	part1(file)
+
+	file, err = os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	part2(file)
 }
